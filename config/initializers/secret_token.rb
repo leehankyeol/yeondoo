c5a14fa3a04efa1711a6c0e29a6777ee34f4719a
Lee Han Kyeol <lee0hankyeol@gmail.com>
2014-12-24 21:27:42 +0900

    initial commit with index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yeondoo::Application.config.secret_key_base = '24da6d60f6bb4d08b72f6f34a47899c9293a4a5bd3afb98b574ee0a561abbb40bd640bfeaf1c17dfa0fc6defea066784667549b6c5cf8f8bee4dd8a8fa5ce815'
