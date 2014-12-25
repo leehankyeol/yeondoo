dc290053b136072d319645a6b45a62650f41d956
Lee Han Kyeol <lee0hankyeol@gmail.com>
2014-12-25 19:27:50 +0900

    added logo & arranged index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,30 @@
+require 'time'
+
 class HomeController < ApplicationController
+
 	def index
-		
+		@isOpen = nil
+		@openHour = nil
+		@closeHour = nil
+		t = Time.new()
+
+		case t.strftime("%A")
+		when "Monday", "Tuesday", "Wednesday", "Thursday"
+			@openHour = Time.new().change(hour: 10)
+			@closeHour = Time.new().change(hour: 22)
+		when "Friday", "Saturday"
+			@openHour = Time.new().change(hour: 11)
+			@closeHour = Time.new().change(hour: 22, min: 30)
+		when "Sunday"
+			@openHour = Time.new().change(hour: 11)
+			@closeHour = Time.new().change(hour: 21)
+		else
+		end
+
+		if (@openHour..@closeHour).cover?(t)
+			@isOpen = true
+		else
+			@isOpen = false
+		end
 	end
 end
