84fb7e2b7dcd2f23e4479c22e73ba5fe17f96660
Lee Han Kyeol <lee0hankyeol@gmail.com>
2014-12-31 03:57:58 +0900

    replaced Facebook API with assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,38 @@ class ApplicationController < ActionController::Base
 		quote_id = rand(Quote.count) + 1
 		@quote = Quote.find(quote_id)
 
-		url = URI.parse('https://graph.facebook.com/v2.2/127100524044142?access_token='+Rails.application.secrets.facebook_page_token)
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		req = Net::HTTP::Get.new(url.to_s)
-		res = http.request(req).body
+		@isOpen = nil
+		@openHour = nil
+		@closeHour = nil
+		t = Time.zone.now
 
-		@yeondoo = JSON(res)
+		case t.strftime("%A")
+		when "Monday", "Tuesday", "Wednesday", "Thursday"
+			@openHour = Time.zone.now.change(hour: 10)
+			@closeHour = Time.zone.now.change(hour: 22)
+		when "Friday", "Saturday"
+			@openHour = Time.zone.now.change(hour: 11)
+			@closeHour = Time.zone.now.change(hour: 22, min: 30)
+		when "Sunday"
+			@openHour = Time.zone.now.change(hour: 11)
+			@closeHour = Time.zone.now.change(hour: 21)
+		else
+		end
+
+		if (@openHour..@closeHour).cover?(t)
+			@isOpen = true
+		else
+			@isOpen = false
+		end
+
+		# TODO: enable Facebook API in the future
+		# url = URI.parse('https://graph.facebook.com/v2.2/127100524044142?access_token='+Rails.application.secrets.facebook_page_token)
+		# http = Net::HTTP.new(url.host, url.port)
+		# http.use_ssl = true
+		# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		# req = Net::HTTP::Get.new(url.to_s)
+		# res = http.request(req).body
+
+		# @yeondoo = JSON(res)
 	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,29 +4,6 @@ require 'time'
 class HomeController < ApplicationController
 
 	def index
-		@isOpen = nil
-		@openHour = nil
-		@closeHour = nil
-		t = Time.zone.now
-
-		case t.strftime("%A")
-		when "Monday", "Tuesday", "Wednesday", "Thursday"
-			@openHour = Time.zone.now.change(hour: 10)
-			@closeHour = Time.zone.now.change(hour: 22)
-		when "Friday", "Saturday"
-			@openHour = Time.zone.now.change(hour: 11)
-			@closeHour = Time.zone.now.change(hour: 22, min: 30)
-		when "Sunday"
-			@openHour = Time.zone.now.change(hour: 11)
-			@closeHour = Time.zone.now.change(hour: 21)
-		else
-		end
-
-		if (@openHour..@closeHour).cover?(t)
-			@isOpen = true
-		else
-			@isOpen = false
-		end
 	end
 
 	def privacy
