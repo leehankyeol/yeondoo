e1c68c4e7345f6de80065e2fc2ea0de0e4dcbba2
Lee Han Kyeol <lee0hankyeol@gmail.com>
2015-01-03 15:32:52 +0900

    replaced Facebook page API with custom styled one

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,17 @@ require 'time'
 
 class HomeController < ApplicationController
 
-	def index
-	end
+  def index
+    url = URI.parse('https://graph.facebook.com/v2.2/ydcoffee/posts?fields=message,object_id,type&access_token='+URI.encode(Rails.application.secrets.facebook_page_token))
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Get.new(url.to_s)
+    res = JSON(http.request(req).body)
 
-	def privacy
-	end
+    @posts = res['data']
+  end
+
+  def privacy
+  end
 end
