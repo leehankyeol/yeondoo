169be2d6436bc60a92066551c3850333fa66484d
Lee Han Kyeol <lee0hankyeol@gmail.com>
2015-01-13 02:03:35 +0900

    changed managing meta tags from controller to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+  	content_for(:description) { page_description }
+  end
+
+  def image(page_image)
+  	content_for(:image) { page_image }
+  end
 end
