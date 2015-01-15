c2f94b09c04e8deb41cefb7c6717e716317aeb92
Lee Han Kyeol <lee0hankyeol@gmail.com>
2015-01-15 16:16:42 +0900

    set locale with params and created locale selection div

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,5 +36,6 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    Rails.application.routes.default_url_options[:locale]= I18n.locale
   end
 end
